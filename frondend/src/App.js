f2a4e4db0c451cd812d3fe41adc8d29eac05c736
Aleesha Shanavas <83001471+AleeshaShanavas@users.noreply.github.com>
2023-01-17 23:29:07 +0530

    Add files via upload

---
+++ frondend/src/App.js
@@ -0,0 +1,16 @@
+// import {useState, useEffect} from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddMovie from './AddMovie'
+
+function App() {
+   
+  return (
+    <div className="App">
+      <AddMovie/>
+      
+    </div>
+  );
+}
+
+export default App;
